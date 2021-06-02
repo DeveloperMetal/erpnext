2f807f44cdde2887129b442cc6121be9ab062734
Felipe Orellana <forellana@digithinkit.com>
2021-06-02 14:26:48 +0000

    fix: js formatting

--- erpnext/templates/includes/integrations/authorizenet_checkout.js
+++ erpnext/templates/includes/integrations/authorizenet_checkout.js
@@ -22,7 +22,7 @@
 *                      another status check request in milliseconds.
 */
 
-frappe.ready(function() {
+frappe.ready(function () {
 	const data = context.replace(/'/g, '"');
 
 	/**
@@ -30,7 +30,7 @@ frappe.ready(function() {
 	 * @param {*} data Order information data structure. See authorizenet_settings.py
 	 * @returns Promise<void>
 	 */
-	const fetch_status = async function(data) {
+	const fetch_status = async function (data) {
 		const r = await frappe.call({
 			method: "erpnext.erpnext_integrations.doctype.authorizenet_settings.authorizenet_settings.get_status",
 			freeze: true,
@@ -52,8 +52,8 @@ frappe.ready(function() {
 	 * @param {int} maxTimeouts Maximum number of timeouts before a hard error
 	 * @returns Promise<void>
 	 */
-	const check_status = async function(data, label, on_success, on_fail, counter, maxTimeouts) {
-		if ( !maxTimeouts) {
+	const check_status = async function (data, label, on_success, on_fail, counter, maxTimeouts) {
+		if (!maxTimeouts) {
 			maxTimeouts = 6; // 6 times x 5 secs = 30secs
 		}
 		// switch ui to display message to user
@@ -64,19 +64,19 @@ frappe.ready(function() {
 			// Process status result
 			await process_status(status, label, on_success, on_fail, counter, maxTimeouts);
 		} catch (err) {
-			if ( err ) {
-				if ( !navigator.onLine || err.status === 0 ) {
-					if ( counter < maxTimeouts ) {
-						setTimeout(() => check_status(data, 
+			if (err) {
+				if (!navigator.onLine || err.status === 0) {
+					if (counter < maxTimeouts) {
+						setTimeout(() => check_status(data,
 							"Detected a network problem. Trying to reconnect, please wait...",
-							on_success, 
-							on_fail, 
-							counter+1), 
-							5000, 
+							on_success,
+							on_fail,
+							counter + 1),
+							5000,
 							maxTimeouts
 						);
 						return; // only early exit when queuing up a delayed check_status.
-					} else if ( on_fail ) {
+					} else if (on_fail) {
 						on_fail({
 							"status": "Failed",
 							"type": "HardError",
@@ -87,7 +87,7 @@ frappe.ready(function() {
 				}
 			}
 
-			if ( on_fail ) {
+			if (on_fail) {
 				on_fail({}, err);
 			}
 		}
@@ -97,7 +97,7 @@ frappe.ready(function() {
 	 * Hides form and displays message label.
 	 * @param {string} label Feedback text to display to the user
 	 */
-	const start_status_check = function(label) {
+	const start_status_check = function (label) {
 		$('#please-wait').text(__(label || "Please Wait..."));
 		$('#please-wait').fadeIn('fast');
 		$('#payment-form').fadeOut('fast');
@@ -106,7 +106,7 @@ frappe.ready(function() {
 	/**
 	 * Displays the credit card form and hides message label.
 	 */
-	const show_form = function() {
+	const show_form = function () {
 		$('#please-wait').fadeOut('fast');
 		$('#payment-form').fadeIn('fast');
 	}
@@ -122,56 +122,56 @@ frappe.ready(function() {
 	 * @param {int} maxTimeouts Maximum number of timeouts before a hard error
 	 * @returns Promise<void>
 	 */
-	const process_status = async function(status, label, on_success, on_fail, counter, maxTimeouts) {
-		if ( !counter ) {
+	const process_status = async function (status, label, on_success, on_fail, counter, maxTimeouts) {
+		if (!counter) {
 			counter = 1;
 		}
-		if ( !maxTimeouts) {
+		if (!maxTimeouts) {
 			maxTimeouts = 6;
 		}
 
-		if ( status.status === "NotFound" ) {
+		if (status.status === "NotFound") {
 			// No previous PR request found. So lets display the UI
 			$('#please-wait').fadeOut('fast');
 			$('#payment-form').fadeIn('fast');
 
-		} else if ( status.status === "Completed" ) {
+		} else if (status.status === "Completed") {
 			// Redirect user to payment success page
-			if ( on_success ) {
+			if (on_success) {
 				await on_success(status);
 			}
 			$('#please-wait').text(__("Payment was successful! Please wait while we redirect you."));
 
 			window.location.href = "/integrations/payment-success";
-		} else if (status.status === "Queued" ) {
+		} else if (status.status === "Queued") {
 			// Processing not completed. Wait for Queue
 			$('#please-wait').text(__("Request is taking longer than expected. Please wait..."));
-			setTimeout(function() { check_status(data, label, on_success, on_fail, counter+1, maxTimeouts) }, status.wait );
+			setTimeout(function () { check_status(data, label, on_success, on_fail, counter + 1, maxTimeouts) }, status.wait);
 
 		} else if (status.status === "Failed") {
-			if ( status.type === "Validation" ) {
+			if (status.type === "Validation") {
 				// On validation errors, re-enable form
 				frappe.msgprint(__(status.description || ""));
 				show_form();
 				$('#submit').prop('disabled', false);
 				$('#submit').html(__('Retry'));
 
-				if ( on_fail ) {
+				if (on_fail) {
 					await on_fail(status);
 				}
-			} else if ( status.type === "HardError" ) {
-				if ( status.description ) {
+			} else if (status.type === "HardError") {
+				if (status.description) {
 					$('#please-wait').text(__(status.description));
 				} else {
 					$('#please-wait').text(__("There was an uhandled internal error. Please try again or contact support."));
 				}
-				if ( on_fail ) {
+				if (on_fail) {
 					await on_fail(status);
 				}
 			} else {
 				// Anything else is a failed request. Lets figure out if its a hard error
 				// or a validation one on the callback
-				if ( on_fail ) {
+				if (on_fail) {
 					await on_fail(status);
 				}
 			}
@@ -184,7 +184,7 @@ frappe.ready(function() {
 	 * @param {onCardProcessFail} on_fail A Failure function callback
 	 * @returns Promise<void>
 	 */
-	const charge = async function(card, on_success, on_fail) {
+	const charge = async function (card, on_success, on_fail) {
 		start_status_check("Processing. Please Wait...");
 		try {
 			const r = await frappe.call({
@@ -195,23 +195,24 @@ frappe.ready(function() {
 					"expiration_date": card.expirationDate,
 					"card_code": card.cardCode,
 					"data": data
-				}});
+				}
+			});
 			await process_status(r.message, "Processing. Please wait...", on_success, on_fail);
-		} catch(err) {
+		} catch (err) {
 			console.error(err);
 			on_fail({}, err);
 		}
 	}
 
 	// Handles credit card submit behaviour
-	$('#submit').on("click", async function(e) {
+	$('#submit').on("click", async function (e) {
 		let data = context.replace(/'/g, '"');
 		e.preventDefault();
 
 		let cardHolderName = document.getElementById('cardholder-name').value;
 		let cardHolderEmail = document.getElementById('cardholder-email').value;
 		let cardNumberWithSpaces = document.getElementById('card-number').value;
-		let cardNumber = cardNumberWithSpaces.replace(/ /g,"");
+		let cardNumber = cardNumberWithSpaces.replace(/ /g, "");
 		let expirationMonth = document.getElementById('card-expiry-month').value;
 		let expirationYear = document.getElementById('card-expiry-year').value;
 		let expirationDate = expirationYear.concat("-").concat(expirationMonth);
@@ -230,7 +231,7 @@ frappe.ready(function() {
 		if (!cardHolderEmail) {
 			frappe.throw(__("Card Holder Email is mandatory."));
 		}
-		
+
 		if (!validate_email(cardHolderEmail)) {
 			frappe.throw(__("Card Holder Email is invalid."));
 		}
@@ -239,15 +240,15 @@ frappe.ready(function() {
 			frappe.throw(__("Card Number is Invalid."));
 		}
 
-		if(cardNumber.length < 13 || cardNumber.length > 16){
+		if (cardNumber.length < 13 || cardNumber.length > 16) {
 			frappe.throw(__("Card Number length should be between 13 and 16 characters"));
 		}
 
-		if(expirationMonth === "00" || expirationMonth.length !== 2 || expirationYear === "0000" || expirationYear.length !== 4){
+		if (expirationMonth === "00" || expirationMonth.length !== 2 || expirationYear === "0000" || expirationYear.length !== 4) {
 			frappe.throw(__("Card Expiration Date is invalid"));
 		}
 
-		if(cardCode.length < 3 || cardCode.length > 4){
+		if (cardCode.length < 3 || cardCode.length > 4) {
 			frappe.throw(__("Card Code length should be between 3 and 4 characters"));
 		}
 
@@ -256,33 +257,33 @@ frappe.ready(function() {
 
 		// issue credit card charge
 		await charge(card, null, async (status, err) => {
-			if ( err ) {
-				if ( !navigator.onLine || err.status === 0 ) {
+			if (err) {
+				if (!navigator.onLine || err.status === 0) {
 					// We lost connection or timed out
 					// Check status until network is back or timeout again
 					// Default should be around 6 times or 30 seconds.
-					check_status(data, 
+					check_status(data,
 						"Detected a network problem. Trying to reconnect, please wait...",
 						null, null, 0);
 				}
 			}
 
-			if ( status.status === "Fail" && status.type === "Duplicate" ) {
+			if (status.status === "Fail" && status.type === "Duplicate") {
 				// Allow up to one more request after a duplicate with a 10 second delay.
 				// This can happen if a card is decliened but the next request is sent before
 				// the duplicate window guard window.
-				setTimeout(() =>charge(card), 10000);
+				setTimeout(() => charge(card), 10000);
 			}
 		});
 	});
 
 	// Ensures only digits are entered on digit fields
 	$('input[data-validation="digit"]')
-		.on("paste", function(e) {
+		.on("paste", function (e) {
 			if (e.originalEvent.clipboardData.getData('text').match(/[^\d]/))
 				e.preventDefault(); //prevent the default behaviour
 		})
-		.keypress(function(event) {
+		.keypress(function (event) {
 			return (event.charCode !== 8 && event.charCode === 0 || (event.charCode >= 48 && event.charCode <= 57));
 		});
 
@@ -297,7 +298,7 @@ frappe.ready(function() {
 	});
 
 	// ping server first to check if this PR was already fullfilled.
-	check_status(data, "Loading...", null, async function(status, err) {
+	check_status(data, "Loading...", null, async function (status, err) {
 		// During any soft failure, display card form.
 		show_form();
 	}, 0);
