61d1fba8e83d996873bc7f42fa6cf58db711e682
Felipe Orellana <1656249+DeveloperMetal@users.noreply.github.com>
2021-06-08 11:31:16 -0400

    Update erpnext/patches/v13_0/set_coupon_code_enabled.py
    Co-authored-by: sahil28297 <37302950+sahil28297@users.noreply.github.com>

--- erpnext/patches/v13_0/set_coupon_code_enabled.py
+++ erpnext/patches/v13_0/set_coupon_code_enabled.py
@@ -2,4 +2,5 @@ import frappe
 
 def execute():
 	# Default existing coupons to enabled
+	frappe.reload_doc("accounts", "doctype", "coupon_code", force=1)
 	frappe.db.sql("UPDATE `tabCoupon Code` SET enabled = 1")
